90b426fcc01d6b37a84bdd15e1fa61aa5b9dd3e0
Kiran Mondal <kiranmondal5516@gmail.com>
2026-06-23 22:13:40 +0530

    Refactored code formatting in `om-core/parser.py` via Telegram Bot Sandbox Engine

--- om-core/parser.py
+++ om-core/parser.py
@@ -2,26 +2,31 @@
 # MODULE: om-core/parser.py
 # =====================================================================
 import sys
-try:
-    from .lexer import TokenType
-except (ImportError, ValueError):
-    from lexer import TokenType
+try: from .lexer import TokenType
+except: from lexer import TokenType
 
 class AssignNode:
-    def __init__(self, name, value): self.name = name; self.value = value
+    def init(self, name, value): self.name = name; self.value = value
 class ShowNode:
-    def __init__(self, expr): self.expr = expr
+    def init(self, expr): self.expr = expr
 class BinOpNode:
-    def __init__(self, left, op, right): self.left = left; self.op = op; self.right = right
+    def init(self, left, op, right): self.left = left; self.op = op; self.right = right
 class NumNode:
-    def __init__(self, val): self.val = val
+    def init(self, val): self.val = val
 class VarNode:
-    def __init__(self, name): self.name = name
+    def init(self, name): self.name = name
 class StringNode:
-    def __init__(self, val): self.val = val
+    def init(self, val): self.val = val
+class InputNode: pass
+
+# NEW NODES
+class IfNode:
+    def init(self, condition, body): self.condition = condition; self.body = body
+class WhileNode:
+    def init(self, condition, body): self.condition = condition; self.body = body
 
 class OmParser:
-    def __init__(self, lexer):
+    def init(self, lexer):
         self.lexer = lexer
         self.current_token = self.lexer.get_next_token()
 
@@ -35,18 +40,47 @@ class OmParser:
     def parse(self):
         statements = []
         while self.current_token.type != TokenType.EOF:
-            if self.current_token.type == TokenType.SHOW:
-                self.consume(TokenType.SHOW)
-                statements.append(ShowNode(self.expr()))
-            elif self.current_token.type == TokenType.IDENTIFIER:
-                name = self.current_token.value
-                self.consume(TokenType.IDENTIFIER)
-                self.consume(TokenType.ASSIGN)
-                statements.append(AssignNode(name, self.expr()))
-            else:
-                print(f"Syntax Error: Invalid start token '{self.current_token.value}'")
-                sys.exit(1)
+            statements.append(self.statement())
         return statements
+
+    def statement(self):
+        if self.current_token.type == TokenType.SHOW:
+            self.consume(TokenType.SHOW)
+            return ShowNode(self.comparison())
+        elif self.current_token.type == TokenType.IF:
+            self.consume(TokenType.IF)
+            condition = self.comparison()
+            body = self.block()
+            return IfNode(condition, body)
+        elif self.current_token.type == TokenType.WHILE:
+            self.consume(TokenType.WHILE)
+            condition = self.comparison()
+            body = self.block()
+            return WhileNode(condition, body)
+        elif self.current_token.type == TokenType.IDENTIFIER:
+            name = self.current_token.value
+            self.consume(TokenType.IDENTIFIER)
+            self.consume(TokenType.ASSIGN)
+            return AssignNode(name, self.comparison())
+        else:
+            print(f"Syntax Error: Invalid token '{self.current_token.value}'")
+            sys.exit(1)
+
+    def block(self):
+        self.consume(TokenType.LBRACE)
+        stmts = []
+        while self.current_token.type not in (TokenType.RBRACE, TokenType.EOF):
+            stmts.append(self.statement())
+        self.consume(TokenType.RBRACE)
+        return stmts
+
+    def comparison(self):
+        node = self.expr()
+        if self.current_token.type in (TokenType.EQ, TokenType.LT, TokenType.GT):
+            op = self.current_token.type
+            self.consume(self.current_token.type)
+            node = BinOpNode(node, op, self.expr())
+        return node
 
     def expr(self):
         node = self.term()
@@ -69,13 +103,3 @@ class OmParser:
         if t.type == TokenType.NUMBER:
             self.consume(TokenType.NUMBER)
             return NumNode(t.value)
-        elif t.type == TokenType.STRING:
-            self.consume(TokenType.STRING)
-            return StringNode(t.value)
-        elif t.type == TokenType.IDENTIFIER:
-            name = t.value
-            self.consume(TokenType.IDENTIFIER)
-            return VarNode(name)
-        print(f"Parser Error: Unexpected token {t}")
-        sys.exit(1)
-        
